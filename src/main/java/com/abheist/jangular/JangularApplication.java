e8473f1afdd2f580b0c4323e35eb71e5cfe5950b
abheist <abhishek291093@gmail.com>
2024-10-14 20:36:32 -0300

    first commit

---
+++ src/main/java/com/abheist/jangular/JangularApplication.java
@@ -0,0 +1,13 @@
+package com.abheist.jangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abheist/jangular/JangularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abheist.jangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
