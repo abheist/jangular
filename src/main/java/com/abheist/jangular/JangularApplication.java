77945d80b6916bad4479132a6a28297d15ef15b3
abheist <abhishek291093@gmail.com>
2024-10-15 10:36:43 -0300

    removed frontend

--- src/main/java/com/abheist/jangular/JangularApplication.java
+++ src/main/java/com/abheist/jangular/JangularApplication.java
@@ -2,12 +2,27 @@ package com.abheist.jangular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 public class JangularApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JangularApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JangularApplication.class, args);
+    }
 
+    @Configuration
+    public class SecurityConfiguration {
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(withDefaults());
+            return http.build();
+        }
+    }
 }
